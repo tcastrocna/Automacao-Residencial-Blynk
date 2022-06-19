2797e8f428882aedf0bd351bbfdb75707bae8edb
Tiago Castro <tiagocastrodacunha3005@gmail.com>
2022-06-19 14:39:42 -0300

    Algumas alterações no código.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# automacaoResidencialBlynk
+# smartHomeESP32v1
 Projeto Simples de automação residencial utilizando a plataforma Blynk. 
-Nesse Projeto utiliza-se a plataforma Blynk, é possivel controlar  os relés atrves do app blynk e  botões físicoa ou controle remoto.
+Nesse Projeto utiliza-se a plataforma Blynk 1.0, é possivel controlar  os relés atrves do app blynk e  botões físicoa ou controle remoto.
+Neste Projeto utiliza-se um módulo relé para controlar os equipamentos eletronicos de um ambiente, no caso o quarto, sendo controle de iluminação, ventilador e tomada.
--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,24 @@
 
 BlynkTimer timer1;
 
+//Auth Token da plataforma Blynk e nome e senha da rede WiFi local.
+#define AUTH "-BTvuCe74-dSR8RMtK8TMxg7fCwqr4_Q"    
+#define WIFI_SSID "Motoe"                         
+#define WIFI_PASS "1A2b3c4d"                     
+
 //Definir os pinos de saidas para os relés
-#define Relay1 18  //Iluminação 1
-#define Relay2 19  //Iluminação 2
-#define Relay3 23  //Ventilador
-#define Relay4 05  //Tomada
+#define pinoRele1 18  //Iluminação 1
+#define pinoRele2 19  //Iluminação 2
+#define pinoRele3 23  //Ventilador
+#define pinoRele4 05  //Tomada
 
 //Definir os pinos de entrada dos botões
-#define Switch1 13  //Iluminação 1
-#define Switch2 12  //Iluminação 2
-#define Switch3 14  //Ventilador
-#define Switch4 27  //Tomada
+#define pinoBotao1 13  //Iluminação 1
+#define pinoBotao2 12  //Iluminação 2
+#define pinoBotao3 14  //Ventilador
+#define pinoBotao4 27  //Tomada
 
-#define IR_RECV_PIN 16 //Sensor IR usado para receber os sinais do controle remoto.
+#define PINO_IR_RECV 16 //Sensor IR usado para receber os sinais do controle remoto.
 
 #define PINO_VIRTUAL1    V1 
 #define PINO_VIRTUAL2    V2
@@ -24,70 +29,66 @@ BlynkTimer timer1;
 #define PINO_VIRTUAL4    V4
 #define PINO_VIRTUAL5    V5
 
-int toggleState_1 = 1; //Defina um inteiro para lembrar o estado de alternância para o relé 1
-int toggleState_2 = 1; //Defina um inteiro para lembrar o estado de alternância para o relé 2
-int toggleState_3 = 1; //Defina um inteiro para lembrar o estado de alternância para o relé 3
-int toggleState_4 = 1; //Defina um inteiro para lembrar o estado de alternância para o relé 4
+int estadoRele_1 = 1; //Defina um inteiro para lembrar o estado de alternância para o relé 1
+int estadoRele_2 = 1; //Defina um inteiro para lembrar o estado de alternância para o relé 2
+int estadoRele_3 = 1; //Defina um inteiro para lembrar o estado de alternância para o relé 3
+int estadoRele_4 = 1; //Defina um inteiro para lembrar o estado de alternância para o relé 4
 
 int wifiFlag = 0;
 
-IRrecv irrecv(IR_RECV_PIN);
+IRrecv irrecv(PINO_IR_RECV);
 decode_results results;
 
-#define AUTH "-BTvuCe74-dSR8RMtK8TMxg7fCwqr4_Q"   //Você deve obter o Auth Token no aplicativo Blynk e colar aqui. 
-#define WIFI_SSID "Motoe"                         //Nome da rede Wifi
-#define WIFI_PASS "1A2b3c4d"                      //Senha da rede Wifi
-
-void relayOnOff(int relay){
+void relayOnOff(int relay ){
     switch(relay){
       case 1: 
-             if(toggleState_1 == 1){
-              digitalWrite(Relay1, LOW); // Liga o relé 1
-              toggleState_1 = 0;
+             if(estadoRele_1 == 1){
+              digitalWrite(pinoRele1, LOW); // Liga o relé 1
+              estadoRele_1 = 0;
               Serial.println("\nRelé 1 ligado");
               }
              else{
-              digitalWrite(Relay1, HIGH); // Desliga o relé 1
-              toggleState_1 = 1;
+              digitalWrite(pinoRele1, HIGH); // Desliga o relé 1
+              estadoRele_1 = 1;
               Serial.println("\nRelé 1 desligado");
               } 
              delay(100);
       break;
       case 2: 
-             if(toggleState_2 == 1){
-              digitalWrite(Relay2, LOW); // Liga o relé 2
-              toggleState_2 = 0;
+             if(estadoRele_2 == 1){
+              digitalWrite(pinoRele2, LOW); // Liga o relé 2
+              estadoRele_2 = 0;
               Serial.println("\nRelé 2 ligado");
               }
              else{
-              digitalWrite(Relay2, HIGH); // Desliga o relé 2
-              toggleState_2 = 1;
+              digitalWrite(pinoRele2, HIGH); // Desliga o relé 2
+              estadoRele_2 = 1;
               Serial.println("\nRelé 2 desligado");
               }
              delay(100);
       break;
        case 3: 
-             if(toggleState_3 == 1){
-              digitalWrite(Relay3, LOW); // Liga o relé 3
-              toggleState_3 = 0;
+             if(estadoRele_3 == 1){
+              digitalWrite(pinoRele3, LOW); // Liga o relé 3
+              estadoRele_3 = 0;
               Serial.println("\nRelé 3 ligado");
               }
              else{
-              digitalWrite(Relay3, HIGH); // Desliga o relé 3
-              toggleState_3 = 1;
+              digitalWrite(pinoRele3, HIGH); // Desliga o relé 3
+              estadoRele_3 = 1;
               Serial.println("\nRelé 3 desligado");
               }
              delay(100);
       break;
       case 4: 
-             if(toggleState_4 == 1){
-              digitalWrite(Relay4, LOW); // Liga o relé 1
-              toggleState_4 = 0;
+             if(estadoRele_4 == 1){
+              digitalWrite(pinoRele4, LOW); // Liga o relé 1
+              estadoRele_4 = 0;
               Serial.println("Relé 4 ligado");
               }
              else{
-              digitalWrite(Relay4, HIGH); // Desliga o relé 1
-              toggleState_4 = 1;
+              digitalWrite(pinoRele4, HIGH); // Desliga o relé 1
+              estadoRele_4 = 1;
               Serial.println("Relé 4 desligado");
               }
              delay(100);
@@ -98,27 +99,27 @@ void relayOnOff(int relay){
 
 void all_SwitchOff()
 {
-  toggleState_1 = 1; digitalWrite(Relay1, toggleState_1);  delay(100);
-  toggleState_2 = 1; digitalWrite(Relay2, toggleState_2);  delay(100);
-  toggleState_3 = 1; digitalWrite(Relay3, toggleState_3);  delay(100);
-  toggleState_4 = 1; digitalWrite(Relay4, toggleState_4);  delay(100);
+  estadoRele_1 = 1; digitalWrite(pinoRele1, estadoRele_1);  delay(100);
+  estadoRele_2 = 1; digitalWrite(pinoRele2, estadoRele_2);  delay(100);
+  estadoRele_3 = 1; digitalWrite(pinoRele3, estadoRele_3);  delay(100);
+  estadoRele_4 = 1; digitalWrite(pinoRele4, estadoRele_4);  delay(100);
 }
 
 void manual_button()
 {
-    if (digitalRead(Switch1) == LOW){
+    if (digitalRead(pinoBotao1) == LOW){
       delay(200);
       relayOnOff(1);      
     }
-    else if (digitalRead(Switch2) == LOW){
+    else if (digitalRead(pinoBotao2) == LOW){
       delay(200);
       relayOnOff(2);
     }
-    else if (digitalRead(Switch3) == LOW){
+    else if (digitalRead(pinoBotao3) == LOW){
       delay(200);
       relayOnOff(3);
     }
-    else if (digitalRead(Switch4) == LOW){
+    else if (digitalRead(pinoBotao4) == LOW){
       delay(200);
       relayOnOff(4);
     }
@@ -141,30 +142,31 @@ void ir_remote(){
 
 // Quando o botão do app é pressionado - muda o estado
 BLYNK_WRITE(PINO_VIRTUAL1) {
-  toggleState_1 = param.asInt();
-  digitalWrite(Relay1, toggleState_1);
+  estadoRele_1 = param.asInt();
+  digitalWrite(pinoRele1, estadoRele_1);
 }
 
 BLYNK_WRITE(PINO_VIRTUAL2) {
-  toggleState_2 = param.asInt();
-  digitalWrite(Relay2, toggleState_2);
+  estadoRele_2 = param.asInt();
+  digitalWrite(pinoRele2, estadoRele_2);
 }
 
 BLYNK_WRITE(PINO_VIRTUAL3) {
-  toggleState_3 = param.asInt();
-  digitalWrite(Relay3, toggleState_3);
+  estadoRele_3 = param.asInt();
+  digitalWrite(pinoRele3, estadoRele_3);
 }
 
 BLYNK_WRITE(PINO_VIRTUAL4) {
-  toggleState_4 = param.asInt();
-  digitalWrite(Relay4, toggleState_4);
+  estadoRele_4 = param.asInt();
+  digitalWrite(pinoRele4, estadoRele_4);
 }
 
 BLYNK_WRITE(PINO_VIRTUAL5) {
   all_SwitchOff();
 }
 
-void checkBlynkStatus() { //Chamado a cada 3 segundos pelo SimpleTimer
+//Chamado a cada 3 segundos pelo SimpleTimer
+void checkBlynkStatus() { 
 
   bool isconnected = Blynk.connected();
   if (isconnected == false) {
@@ -173,10 +175,10 @@ void checkBlynkStatus() { //Chamado a cada 3 segundos pelo SimpleTimer
   }
   if (isconnected == true) {
     wifiFlag = 0;
-    Blynk.virtualWrite(PINO_VIRTUAL1, toggleState_1);
-    Blynk.virtualWrite(PINO_VIRTUAL2, toggleState_2);
-    Blynk.virtualWrite(PINO_VIRTUAL3, toggleState_3);
-    Blynk.virtualWrite(PINO_VIRTUAL4, toggleState_4);
+    Blynk.virtualWrite(PINO_VIRTUAL1, estadoRele_1);
+    Blynk.virtualWrite(PINO_VIRTUAL2, estadoRele_2);
+    Blynk.virtualWrite(PINO_VIRTUAL3, estadoRele_3);
+    Blynk.virtualWrite(PINO_VIRTUAL4, estadoRele_4);
 
     digitalWrite(LED_BUILTIN, HIGH); //Liga o led quando existir rede wifi.
   }
@@ -187,23 +189,23 @@ void setup()
   
   irrecv.enableIRIn(); // Start the receiver
   
-  pinMode(Relay1, OUTPUT);
-  pinMode(Relay2, OUTPUT);
-  pinMode(Relay3, OUTPUT);
-  pinMode(Relay4, OUTPUT);
+  pinMode(pinoRele1, OUTPUT);
+  pinMode(pinoRele2, OUTPUT);
+  pinMode(pinoRele3, OUTPUT);
+  pinMode(pinoRele4, OUTPUT);
 
   pinMode(LED_BUILTIN, OUTPUT);
 
-  pinMode(Switch1, INPUT_PULLUP);
-  pinMode(Switch2, INPUT_PULLUP);
-  pinMode(Switch3, INPUT_PULLUP);
-  pinMode(Switch4, INPUT_PULLUP);
+  pinMode(pinoBotao1, INPUT_PULLUP);
+  pinMode(pinoBotao2, INPUT_PULLUP);
+  pinMode(pinoBotao3, INPUT_PULLUP);
+  pinMode(pinoBotao4, INPUT_PULLUP);
     
   //Durante a partida todos os relés devem DESLIGAR
-  digitalWrite(Relay1, toggleState_1);
-  digitalWrite(Relay2, toggleState_2);
-  digitalWrite(Relay3, toggleState_3);
-  digitalWrite(Relay4, toggleState_4);
+  digitalWrite(pinoRele1, estadoRele_1);
+  digitalWrite(pinoRele2, estadoRele_2);
+  digitalWrite(pinoRele3, estadoRele_3);
+  digitalWrite(pinoRele4, estadoRele_4);
  
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   timer1.setInterval(2000L, checkBlynkStatus); //Verifica se o servidor Blynk está conectado a cada 3 segundos
@@ -211,10 +213,10 @@ void setup()
   delay(1000);
 
 
-  Blynk.virtualWrite(PINO_VIRTUAL1, toggleState_1);
-  Blynk.virtualWrite(PINO_VIRTUAL2, toggleState_2);
-  Blynk.virtualWrite(PINO_VIRTUAL3, toggleState_3);
-  Blynk.virtualWrite(PINO_VIRTUAL4, toggleState_4);
+  Blynk.virtualWrite(PINO_VIRTUAL1, estadoRele_1);
+  Blynk.virtualWrite(PINO_VIRTUAL2, estadoRele_2);
+  Blynk.virtualWrite(PINO_VIRTUAL3, estadoRele_3);
+  Blynk.virtualWrite(PINO_VIRTUAL4, estadoRele_4);
 }
 
 void loop()
@@ -227,5 +229,5 @@ void loop()
     Blynk.run();
   }
   
-  timer1.run(); // Inicia o SimpleTimer
+  timer1.run();
 }
